9ec1f26aac08dcc7dbe5d72fd2f9a5d0a32a0bcc
Liu Xiang <liuxiang@ktjr.com>
2019-03-01 11:03:55 +0800

    Revise ruby debugger keywords matching regexp

--- layers/+lang/ruby/funcs.el
+++ layers/+lang/ruby/funcs.el
@@ -67,6 +67,6 @@ Called interactively it prompts for a directory."
   "Highlight break point lines."
   (interactive)
   (when ruby-highlight-debugger-keywords
-    (highlight-lines-matching-regexp "byebug")
-    (highlight-lines-matching-regexp "binding.irb")
-    (highlight-lines-matching-regexp "binding.pry")))
+    ;; (highlight-lines-matching-regexp "byebug")
+    (highlight-lines-matching-regexp ".*binding.irb.*")
+    (highlight-lines-matching-regexp ".*binding.pry.*")))
