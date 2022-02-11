e749e89386cefb8f490d6d1594bfafa6801ee26b
Liu Xiang <liuxiang@ktjr.com>
2022-02-11 16:47:38 +0800

    Improve spacemacs buffer recentf list rendering performance

--- core/core-spacemacs-buffer.el
+++ core/core-spacemacs-buffer.el
@@ -1186,21 +1186,18 @@ SEQ, START and END are the same arguments as for `cl-subseq'"
     (insert spacemacs-buffer-list-separator)))
 
 (defun spacemacs-buffer//insert-recent-files (list-size)
-  (unless recentf-mode (recentf-mode))
-  (setq spacemacs-buffer//recent-files-list
-        (cl-delete-if (lambda (x)
-                        (or (when (and (bound-and-true-p org-directory) (file-exists-p org-directory))
-                              (member x (directory-files org-directory t)))
-                            (when (bound-and-true-p org-agenda-files)
-                              (member x (mapcar #'expand-file-name (org-agenda-files))))))
-                      recentf-list))
-  (setq spacemacs-buffer//recent-files-list
-        (spacemacs//subseq spacemacs-buffer//recent-files-list 0 list-size))
-  (when (spacemacs-buffer//insert-file-list
-         "Recent Files:"
-         spacemacs-buffer//recent-files-list)
-    (spacemacs-buffer||add-shortcut "r" "Recent Files:"))
-  (insert spacemacs-buffer-list-separator))
+  (let ((agenda-files (org-agenda-files)))
+    (unless recentf-mode (recentf-mode))
+    (setq spacemacs-buffer//recent-files-list
+          (cl-delete-if (lambda (x) (member x (mapcar #'expand-file-name agenda-files)))
+                        recentf-list))
+    (setq spacemacs-buffer//recent-files-list
+          (spacemacs//subseq spacemacs-buffer//recent-files-list 0 list-size))
+    (when (spacemacs-buffer//insert-file-list
+           "Recent Files:"
+           spacemacs-buffer//recent-files-list)
+      (spacemacs-buffer||add-shortcut "r" "Recent Files:"))
+    (insert spacemacs-buffer-list-separator)))
 
 (defun spacemacs-buffer//insert-recent-files-by-project (list-size)
   (unless recentf-mode (recentf-mode))
