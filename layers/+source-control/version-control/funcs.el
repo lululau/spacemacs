b325acfd69b31b86e33533c518e6a24bcd3cee61
Liu Xiang <liuxiang921@gmail.com>
2025-05-20 11:09:33 +0800

    Fix version control margin checks for unbound variables
    - Replace direct variable access with `bound-and-true-p` to handle unbound cases
    - Update `spacemacs/vcs-margin-p` and `spacemacs/vcs-margin-global-p` functions
    - Prevent potential errors when diff tools are not properly initialized

--- layers/+source-control/version-control/funcs.el
+++ layers/+source-control/version-control/funcs.el
@@ -109,14 +109,14 @@
 (defun spacemacs/vcs-margin-p ()
   (interactive)
   (cl-case version-control-diff-tool
-    (diff-hl     diff-hl-margin-local-mode)
+    (diff-hl     (bound-and-true-p diff-hl-margin-local-mode))
     (git-gutter  (bound-and-true-p git-gutter-mode))))
 
 (defun spacemacs/vcs-margin-global-p ()
   (interactive)
   (cl-case version-control-diff-tool
-    (diff-hl     diff-hl-margin-mode)
-    (git-gutter  global-git-gutter-mode)))
+    (diff-hl     (bound-and-true-p diff-hl-margin-mode))
+    (git-gutter  (bound-and-true-p global-git-gutter-mode))))
 
 (spacemacs|add-toggle version-control-margin
   :status (spacemacs/vcs-margin-p)
