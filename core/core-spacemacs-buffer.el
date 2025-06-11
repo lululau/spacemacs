3efc5487d444e6941a7d5886f4b39c9006ccf275
Liu Xiang <liuxiang921@gmail.com>
2025-06-11 13:47:59 +0800

    Do not load org.el during startup

--- core/core-spacemacs-buffer.el
+++ core/core-spacemacs-buffer.el
@@ -1318,6 +1318,53 @@ SEQ, START and END are the same arguments as for `cl-subseq'"
     (spacemacs-buffer||add-shortcut "w" "Warnings:")
     (insert spacemacs-buffer-list-separator)))
 
+;; (defun spacemacs-buffer//insert-recent-files (list-size)
+;;   "Insert recent file entries to spacemacs-buffer.
+
+;; LIST-SIZE is specified in `dotspacemacs-startup-lists' for recent entries."
+;;   (unless recentf-mode (recentf-mode))
+;;   (let (;; we need to remove `org-agenda-files' entries from recent files
+;;         (agenda-files
+;;          (when-let* ((default-directory
+;;                       (or (bound-and-true-p org-directory) "~/org"))
+;;                      (files
+;;                       (when (bound-and-true-p org-agenda-files)
+;;                         (if (listp org-agenda-files)
+;;                             ;; if it's a list, we take that value directly
+;;                             org-agenda-files
+;;                           ;; but if it's a string, it must be file where the list
+;;                           ;; of agenda files are stored in that file and we have
+;;                           ;; to load `org-agenda' to process the list. If org is
+;;                           ;; already loaded, then we assume that the user has
+;;                           ;; already called org-agenda-files.
+;;                           (when (not (featurep 'org))
+;;                             (warn "`org-agenda-files' is a string and \
+;; not a list. This requires us to load `org' to process the org agenda files in \
+;; startup list.")
+;;                             (require 'org)
+;;                             (org-agenda-files))))))
+;;            (mapcar #'expand-file-name files)))
+;;         ;; we also need to skip sub-directories of `org-directory'
+;;         (ignore-directory (when (bound-and-true-p org-directory)
+;;                             (expand-file-name org-directory)))
+;;         (recent-files-list))
+;;     (cl-loop for rfile in recentf-list
+;;              while (> list-size 0)
+;;              do (let ((full-path (expand-file-name rfile)))
+;;                   (unless (or (and ignore-directory
+;;                                    (string-prefix-p ignore-directory full-path))
+;;                               (member full-path agenda-files))
+;;                     (cl-pushnew rfile recent-files-list)
+;;                     (setq list-size (1- list-size))))
+;;              finally do (setq recent-files-list (nreverse recent-files-list)))
+;;     (when (spacemacs-buffer//insert-file-list
+;;            (spacemacs-buffer||propertize-heading
+;;             (spacemacs-buffer//font-icons-icon "" 'history)
+;;             "Recent Files:" "r")
+;;            recent-files-list)
+;;       (spacemacs-buffer||add-shortcut "r" "Recent Files:")))
+;;   (insert spacemacs-buffer-list-separator))
+
 (defun spacemacs-buffer//insert-recent-files (list-size)
   "Insert recent file entries to spacemacs-buffer.
 
@@ -1337,12 +1384,7 @@ LIST-SIZE is specified in `dotspacemacs-startup-lists' for recent entries."
                           ;; to load `org-agenda' to process the list. If org is
                           ;; already loaded, then we assume that the user has
                           ;; already called org-agenda-files.
-                          (when (not (featurep 'org))
-                            (warn "`org-agenda-files' is a string and \
-not a list. This requires us to load `org' to process the org agenda files in \
-startup list.")
-                            (require 'org)
-                            (org-agenda-files))))))
+                          ))))
            (mapcar #'expand-file-name files)))
         ;; we also need to skip sub-directories of `org-directory'
         (ignore-directory (when (bound-and-true-p org-directory)
